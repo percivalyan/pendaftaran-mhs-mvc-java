62586fd1ef3f242e74b6f094dbf5687cc7f69886
percivalyan <yoryan8888@gmail.com>
2023-11-06 23:35:14 +0700

    Programming 1, Abstract Class, Java Interface, NetBeans

---
+++ Documents/NetBeansProjects/pertemuan9/src/main/java/com/mycompany/javainterface/Relation.java
@@ -0,0 +1,7 @@
+package com.mycompany.javainterface;
+
+public interface Relation {
+   public boolean isGreater( Object a, Object b);
+   public boolean isLess( Object a, Object b);
+   public boolean isEqual( Object a, Object b);
+} 
