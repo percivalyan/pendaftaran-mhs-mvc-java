4329fd3894ec090016cbf7e19048b0149ed1273a
percivalyan <150053025+percivalyan@users.noreply.github.com>
2023-12-21 14:26:07 +0700

    Delete Documents/NetBeansProjects/pertemuan9 directory

--- Documents/NetBeansProjects/pertemuan9/src/main/java/com/mycompany/javainterface/Relation.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.javainterface;
-
-public interface Relation {
-   public boolean isGreater( Object a, Object b);
-   public boolean isLess( Object a, Object b);
-   public boolean isEqual( Object a, Object b);
-} 
